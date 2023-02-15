9c0d5b28922ff1dc06b35a3d8f20cf8b03708af1
texasnk <davirodriguesnk@gmail.com>
2023-02-15 14:23:59 -0300

    Add Fibonacci files

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write a number for the fibonacci sequence: ");
+            long fibo = long.Parse(Console.ReadLine()), a=0, b=1, c=0;
+   
+            for(int i=0;i<fibo; i++)
+            {
+                if (i < fibo - 1)
+                {
+                    Console.Write(a + ", ");
+                }
+                else
+                {
+                    Console.Write(a);
+                }
+                c = a + b;
+                a = b;
+                b = c;
+
+
+            }
+
+    
+
+            Console.ReadKey();
+        }
+    }
+}
